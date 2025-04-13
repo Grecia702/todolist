9bba1dd70c1cc6b4afe46087983cf3a6fd2d93dd
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-04-12 21:32:06 -0400

    feat: armazenamento em local storage
    - adiciona funcionalidade de armazenamento de task no local storage
    - atualiza a UI da página

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
+const localStoragekey = "todo-list"
+
 function criarTarefa() {
-    let x = document.getElementById('name').value
-    if (x != '') {
-
-        const textbox = document.createElement("div");
-        textbox.innerText = x;
-        textbox.className = 'textbox'
-
-        let task = document.createElement("div")
-        task.className = 'task'
-        task.appendChild(textbox)
-        let tarefa = document.getElementById('tarefa')
-        tarefa.appendChild(task)
-
-
-        let divImg = document.createElement("div")
-        task.appendChild(divImg)
-        divImg.classList.add("NewTask")
-
-        const { icon: check } = createIcon(divImg, 'icons/check-mark.png', 'check');
-        const { icon: apagar } = createIcon(divImg, 'icons/bin.png', 'delete')
-        document.getElementById('name').value = '';
-        check.onclick = function () {
-            textbox.classList.toggle('taskCompleted')
-        }
-        apagar.onclick = function () {
-            task.remove()
-        }
-    } else {
+    let newTask = document.getElementById('name').value
+    if (newTask == '') {
         alert("Insira uma tarefa")
+        return;
+    }
+    const textbox = document.createElement("div");
+    textbox.innerText = newTask;
+    textbox.className = 'textbox'
+
+    let values = JSON.parse(localStorage.getItem(localStoragekey) || "[]")
+    values.push({
+        id: values.length,
+        value: newTask,
+        checked: false
+    })
+    localStorage.setItem(localStoragekey, JSON.stringify(values))
+    loadTasks();
+}
+
+function loadTasks() {
+    let values = JSON.parse(localStorage.getItem(localStoragekey) || '[]')
+    let list = document.getElementById('tarefa')
+    let imgSrc = "icons/trash.svg"
+    list.innerHTML = ''
+
+    for (let i = 0; i < values.length; i++) {
+        list.innerHTML += `
+        <div class="${values[i]['checked'] ? "task taskCompleted" : "task"}" id="task${i}">
+            <input type="checkbox" class="check-box" id="complete${i}" ${values[i]['checked'] ? 'checked' : ''}>
+            <div class="textbox">${values[i]['value']}</div>
+            <label for="complete${i}" class="check-label"></label>
+            <div class="DeleteTask">
+            <img src="${imgSrc}" class="delete" id="delete${i}" />
+            </div>
+        </div>
+        `
+    }
+
+    for (let i = 0; i < values.length; i++) {
+        const base = document.querySelector('.pesquisar');
+        const alvo = document.getElementById(`task${i}`);
+        const larguraBase = base.offsetWidth;
+        const deleteTask = document.getElementById(`delete${i}`)
+        const completeTask = document.getElementById(`complete${i}`)
+        alvo.style.maxWidth = `${larguraBase}px`;
+
+        deleteTask.addEventListener("click", () => {
+            alvo.remove()
+            let values = JSON.parse(localStorage.getItem(localStoragekey) || "[]")
+            let idRemove = i
+            values = values.filter(task => task.id !== idRemove)
+            localStorage.setItem(localStoragekey, JSON.stringify(values));
+        });
+
+        completeTask.addEventListener("click", () => {
+            let values = JSON.parse(localStorage.getItem(localStoragekey) || "[]")
+            checar = values.find(task => task.id === i)
+            checar.checked = !checar.checked
+            alvo.classList.toggle('taskCompleted', alvo.checked)
+            localStorage.setItem(localStoragekey, JSON.stringify(values));
+        });
     }
 }
-function createIcon(div, source, type) {
-    icon = document.createElement('img')
-    icon.src = source
-    icon.className = type
-    div.appendChild(icon)
-    return {
-        icon,
-        div
-    };
-}
+
+loadTasks();
